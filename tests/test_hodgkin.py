b149a99c44bdc8355008065f2ee146908e288950
Asif Tamuri <tamuri@gmail.com>
2018-11-08 17:22:55 +0000

    Add tests based around Hodgkin 1952 model

---
+++ tests/test_hodgkin.py
@@ -0,0 +1,46 @@
+import logging
+import os
+
+import pytest
+
+from cellmlmanip import parser
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+class TestHodgkin:
+    @pytest.fixture(scope="class")
+    def model(self):
+        hodgkin_cellml = os.path.join(
+            os.path.dirname(__file__),
+            "cellml_files",
+            "hodgkin_huxley_squid_axon_model_1952_modified.cellml"
+        )
+        p = parser.Parser(hodgkin_cellml)
+        model = p.parse()
+        return model
+
+    def test_counts(self, model):
+        # https://models.cellml.org/exposure/5d116522c3b43ccaeb87a1ed10139016/hodgkin_huxley_1952_variant01.cellml/cellml_math
+        assert len(model.components) == 8
+        eq_count = sum([len(c.equations) for c in model.components.values()])
+        assert eq_count == 17
+
+    def test_setup_connections(self, model):
+        model.make_connections()
+        target = model.components['sodium_channel'].variables['h']
+        source = model.components['sodium_channel_h_gate'].variables['h']
+        assert target['assignment'] == source['sympy.Dummy']
+
+    def test_add_units_to_equations(self, model):
+        for c in model.components.values():
+            c.add_units_to_equations()
+        equation = model.components['sodium_channel'].equations[0]
+        lhs_units = model.units.summarise_units(equation.lhs)
+        assert lhs_units == model.units.store['millivolt']
+
+    def test_check_left_right_units(self, model):
+        for c in model.components.values():
+            for e in c.equations:
+                model.check_left_right_equality_units(e)
+
