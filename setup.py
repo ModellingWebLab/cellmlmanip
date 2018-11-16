5cd36531b6bf27ba7dfc9612a239b0b5a3c0f775
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-11-16 21:49:29 +0000

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -15,5 +15,17 @@ setup(
     author_email='',
     url='https://github.com/ModellingWebLab/cellmlmanip',
     license=license_,
-    find_packages=find_packages(exclude=('tests', 'docs'))
+    find_packages=find_packages(exclude=('tests', 'docs')),
+    
+    install_requires=[
+        'lxml>=4',
+        'networkx>=2',
+        'rdflib>=4',
+        'sympy==1.1.1',
+    ],
+    extras_require={
+        'test': [
+            'pytest>=3.2',
+        ],
+    },
 )
