d37750bd42b90b9f50249109ea7a5a075807bf88
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-02-28 16:41:45 +0000

    Added tests for create_rdf_node

--- tests/test_rdf.py
+++ tests/test_rdf.py
@@ -15,9 +15,15 @@ def model():
 
 def test_create_rdf_node():
     # Tests rdf.create_rdf_node
-    # TODO
-    pass
 
+    node = cellmlmanip.rdf.create_rdf_node('http://example.com/', 'hi')
+    assert str(node) == 'http://example.com/hi'
+
+    node = cellmlmanip.rdf.create_rdf_node('http://example.com#', 'hi')
+    assert str(node) == 'http://example.com#hi'
+
+    node = cellmlmanip.rdf.create_rdf_node('http://example.com', 'hi')
+    assert str(node) == 'http://example.com#hi'
 
 def test_get_symbol_by_ontology_term(model):
     # Tests model.get_symbol_by_ontology_term
@@ -27,3 +33,9 @@ def test_get_symbol_by_ontology_term(model):
     var = model.get_symbol_by_ontology_term(oxmeta, 'time')
     assert isinstance(var, sympy.Symbol)
     assert var.name == 'environment$time'
+
+    # Test getting a non-existent variable
+    with pytest.raises(KeyError):
+        model.get_symbol_by_ontology_term(oxmeta, 'bert')
+    with pytest.raises(KeyError):
+        model.get_symbol_by_ontology_term('http://example.com#', 'time')
