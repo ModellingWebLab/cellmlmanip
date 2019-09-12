86cb61f9137450520615b97a711fc1c0c8cc1f83
Sarah <s.keating@ucl.ac.uk>
2019-09-12 12:30:38 +0100

    fix test having resolved my understanding of what function does

--- tests/test_model_units.py
+++ tests/test_model_units.py
@@ -4,7 +4,7 @@ import os
 from cellmlmanip import parser
 
 
-class TestModelUnits():
+class TestModelUnits:
     @pytest.fixture(scope="class")
     def parser_instance(self):
         """Parses example CellML and returns model"""
@@ -28,33 +28,35 @@ class TestModelUnits():
 
     def test_equations(self, parser_instance, model):
         model.get_equation_graph(True)  # set up the graph - it is not automatic
-        symbolA = model.get_symbol_by_cmeta_id("a")
-        equation = model.get_equations_for([symbolA])
+        symbol_a = model.get_symbol_by_cmeta_id("a")
+        equation = model.get_equations_for([symbol_a])
         assert len(equation) == 1
-        assert equation[0].lhs == symbolA
+        assert equation[0].lhs == symbol_a
         assert equation[0].rhs == 1.0
 
-        symbolB = model.get_symbol_by_cmeta_id("b")
-        # Note calling this function a second time adds to the list of equations
-        # I'm not sure that is expected behaviour -
-        # I have only asked for equations for 'b'
-        equation1 = model.get_equations_for([symbolB])
-        assert len(equation1) == 2
-        assert equation1[1].lhs == symbolB
-        assert equation1[1].rhs == 2.0 / symbolA
+    def test_equations_2(self, parser_instance, model):
+        model.get_equation_graph(True)  # set up the graph - it is not automatic
+        symbol_a = model.get_symbol_by_cmeta_id("a")
+        symbol_b = model.get_symbol_by_cmeta_id("b")
+        equation = model.get_equations_for([symbol_b])
+        assert len(equation) == 2
+        assert equation[0].lhs == symbol_a
+        assert equation[0].rhs == 1.0
+        assert equation[1].lhs == symbol_b
+        assert equation[1].rhs == 2.0 / symbol_a
 
     def test_units(self, parser_instance, model):
         model.get_equation_graph(True)  # set up the graph - it is not automatic
-        symbolA = model.get_symbol_by_cmeta_id("a")
-        equation = model.get_equations_for([symbolA])
+        symbol_a = model.get_symbol_by_cmeta_id("a")
+        equation = model.get_equations_for([symbol_a])
         assert model.units.summarise_units(equation[0].lhs) == 'ms'
         # this fails but it should not
         # the number 1 has clearly marked units of 'ms'
         # NOTE it fails in the function UnitCalculator::traverse
 #        assert model.units.summarise_units(equation[0].rhs) == 'ms'
 
-        symbolB = model.get_symbol_by_cmeta_id("b")
-        equation = model.get_equations_for([symbolB])
+        symbol_b = model.get_symbol_by_cmeta_id("b")
+        equation = model.get_equations_for([symbol_b])
         assert model.units.summarise_units(equation[1].lhs) == 'per_ms'
         # this fails in the function UnitCalculator::traverse
 #        assert model.units.summarise_units(equation[1].rhs) == 'per_ms'
