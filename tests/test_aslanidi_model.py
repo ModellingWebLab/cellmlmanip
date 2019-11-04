c2b644044162ca8da7155f1ee66c511264a60ae5
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2019-11-04 20:59:58 +0000

    text fix

--- tests/test_aslanidi_model.py
+++ tests/test_aslanidi_model.py
@@ -38,10 +38,17 @@ class TestAslanidiModel:
         for i in range(len(sorted_derivs)):
             assert sorted_derivs[i] == sorted_derivs_order[i]
 
-        print(str(derivs))
-        print("---\n")
-        print(str(derivs_order))
-        print("derivs_order")
-        assert False
-        assert derivs[16] == derivs_order[17]
-        assert derivs[17] == derivs_order[16]
+        str(derivs_order) == '[_membrane$V, _sodium_current_m_gate$m, _sodium_current_h1_gate$h1, \
+            _sodium_current_h2_gate$h2, _L_type_Ca_channel_d_L_gate$d_L, _L_type_Ca_channel_f_L_gate$f_L, \
+            _T_type_Ca_channel_d_T_gate$d_T, _T_type_Ca_channel_f_T_gate$f_T, \
+            _Ca_independent_transient_outward_K_current_r_gate$r, \
+            _Ca_independent_transient_outward_K_current_s1_gate$s1, \
+            _Ca_independent_transient_outward_K_current_s2_gate$s2, \
+            _Ca_independent_transient_outward_K_current_s3_gate$s3, \
+            _delayed_rectifier_K_current_z_gate$z, _delayed_rectifier_K_current_pa_gate$p_a, \
+            _delayed_rectifier_K_current_pi_gate$p_i, _intracellular_ion_concentrations$Na_i, \
+            _intracellular_ion_concentrations$Ca_i, _intracellular_ion_concentrations$K_i, \
+            _intracellular_Ca_buffering$O_C, _intracellular_Ca_buffering$O_TC, _intracellular_Ca_buffering$O_TMgC, \
+            _intracellular_Ca_buffering$O_TMgMg, _cleft_space_ion_concentrations$K_c, _Ca_handling_by_the_SR$Ca_rel, \
+            _Ca_handling_by_the_SR$Ca_up, _Ca_handling_by_the_SR$O_Calse, _Ca_handling_by_the_SR$F1, \
+            _Ca_handling_by_the_SR$F2, _Ca_handling_by_the_SR$F3]'
