7d7488f33ef69cfa0b23460255eff71d152d244e
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2019-11-05 11:46:57 +0000

    fixed test

--- tests/test_aslanidi_model.py
+++ tests/test_aslanidi_model.py
@@ -36,10 +36,8 @@ class TestAslanidiModel:
         _potassium_channel_n_gate$n
         """
         state_symbols = model.get_state_symbols()
-        state_symbols_ordered_by_input = model.get_state_symbols(order_by_order_added=True)
-        assert len(state_symbols) == len(state_symbols_ordered_by_input) == 29
-        assert set(state_symbols) == set(state_symbols_ordered_by_input)
-        assert str(state_symbols_ordered_by_input) == \
+        assert len(state_symbols) == 29
+        assert str(state_symbols) == \
             '[_membrane$V, _sodium_current_m_gate$m, _sodium_current_h1_gate$h1, _sodium_current_h2_gate$h2, '\
             '_L_type_Ca_channel_d_L_gate$d_L, _L_type_Ca_channel_f_L_gate$f_L, '\
             '_T_type_Ca_channel_d_T_gate$d_T, _T_type_Ca_channel_f_T_gate$f_T, '\
