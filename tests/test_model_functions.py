3673a0470b15f74d8d394f0ea86437a782d50497
Sarah <s.keating@ucl.ac.uk>
2019-11-13 16:17:24 +0000

    adding tests for model class functions

--- tests/test_model_functions.py
+++ tests/test_model_functions.py
@@ -1,23 +1,61 @@
 import os
+from collections import OrderedDict
 
 import pytest
-import sympy
 
 import cellmlmanip
 import cellmlmanip.rdf
-
-def load_model(name):
-    """ Parses and returns one of the CellML test models """
-    if name[-7:] != '.cellml':
-        name += '.cellml'
-    return cellmlmanip.load_model(os.path.join(
-        os.path.dirname(__file__), 'cellml_files', name))
+from cellmlmanip.units import UnitStore
 
 
+class TestModelFunctions():
+    # These represent CellML <units><unit>...</unit></units> elements
+    test_definitions = OrderedDict({
+        'ms': [{'units': 'second', 'prefix': 'milli'}],
+        'usec': [{'units': 'second', 'prefix': 'micro'}],
+        'mV': [{'units': 'volt', 'prefix': 'milli'}],
+        'uV': [{'units': 'volt', 'prefix': 'micro'}],
+        'mM': [{'prefix': 'milli', 'units': 'mole'}, {'units': 'litre', 'exponent': '-1'}],
+        'milli_mole': [{'prefix': 'milli', 'units': 'mole'}],
+        'millisecond': [{'prefix': 'milli', 'units': 'second'}],
+    })
+    test_definitions.update({
+        'per_ms': [{'units': 'ms', 'exponent': '-1'}],
+        'per_mV': [{'units': 'volt', 'prefix': 'milli', 'exponent': '-1'}],
+        'mV_per_ms': [{'units': 'mV', 'exponent': '1'}, {'units': 'ms', 'exponent': '-1'}],
+        'mV_per_s': [{'units': 'mV', 'exponent': '1'}, {'units': 'second', 'exponent': '-1'}],
+        'mV_per_usec': [
+            {'units': 'mV', 'exponent': '1'}, {'prefix': 'micro', 'units': 'second', 'exponent': '-1'}],
+        'mM_per_ms': [{'units': 'mM'}, {'units': 'ms', 'exponent': '-1'}],
+        'ms_power_prefix': [{'prefix': '-3', 'units': 'second'}],
+        'ms_with_multiplier': [{'multiplier': 0.001, 'units': 'second'}],
+    })
 
-def test_get_state_symbols():
-    model = load_model('aslanidi_model_2009')
-    model.get_equation_graph()
-    state_symbols = model.get_state_symbols()
-    assert len(state_symbols) == 29
+    @pytest.fixture
+    def model(scope='class'):
+        return cellmlmanip.load_model(
+            os.path.join(os.path.dirname(__file__), 'cellml_files', "beeler_reuter_model_1977.cellml"))
+
+    @pytest.fixture(scope="class")
+    def graph(self, model):
+        return model.get_equation_graph()
+
+    @pytest.fixture(scope="class")
+    def quantity_store(self, model):
+        qs = UnitStore(model)
+        for unit_name, unit_attributes in self.test_definitions.items():
+            qs.add_custom_unit(unit_name, unit_attributes)
+        return qs
+
+    def test_get_state_symbols(self, model):
+        state_symbols = model.get_state_symbols()
+        assert len(state_symbols) == 8
+
+    def test_get_derivative_symbols(self, model):
+        derivs = model.get_derivative_symbols()
+        assert len(derivs) == 8
+
+    def test_free_variable_symbol(self, model):
+        free_variable_symbol = model.get_free_variable_symbol()
+        assert free_variable_symbol.name == 'environment$time'
 
