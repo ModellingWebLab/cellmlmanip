0a89772fc36daa675ab16bcf8842ec75e4dd0f48
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-11-21 22:35:16 +0000

    Fixed typo in cover pragma

--- cellmlmanip/units.py
+++ cellmlmanip/units.py
@@ -498,7 +498,7 @@ class UnitCalculator(object):
                 else:
                     # otherwise, keep the symbol
                     return self.ureg.Quantity(expr, expr.units)
-            else:   # pramga: no cover
+            else:   # pragma: no cover
                 # An unexpected type of symbol: print anyway for debugging
                 return str(expr)
         elif expr == sympy.oo:
