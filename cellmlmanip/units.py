72b59a68f7a77bed59c4ceed1156975d2dc333df
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2020-05-14 21:21:36 +0100

    Use only a single context

--- cellmlmanip/units.py
+++ cellmlmanip/units.py
@@ -131,6 +131,9 @@ class UnitStore(object):
         # Create a unit calculator
         self._calculator = UnitCalculator(self)
 
+        # Used for conversion rules added by add_conversion_rule
+        self._context = None
+
         # Expose the Unit and Quantity classes
         # TODO Might not be needed in 0.10 anymore
         self.Unit = self._registry.Unit
@@ -282,10 +285,20 @@ class UnitStore(object):
 
     def add_conversion_rule(self, from_unit, to_unit, rule):
         """Adds complex conversion rule for converting between incompatible units."""
-        context = pint.Context(str(from_unit) + str(to_unit) + str(rule))
-        context.add_transformation(from_unit, to_unit, rule)
-        self._registry.add_context(context)
-        self._registry.enable_contexts(str(from_unit) + str(to_unit) + str(rule))
+        if self._context is not None:
+            # We've already added another rule, so disable the existing context and remember its rules
+            self._registry.disable_contexts()
+            old_funcs = self._context.funcs
+        else:
+            # No existing rules to copy over
+            old_funcs = {}
+        # Create a new context
+        self._context = pint.Context('user_conversion_rules')
+        for src_dest, func in old_funcs:
+            self._context.add_transformation(src_dest[0], src_dest[1], func)
+        # Now add the new rule and enable the context
+        self._context.add_transformation(from_unit, to_unit, rule)
+        self._registry.enable_contexts(self._context)
 
     def evaluate_units_and_fix(self, expr):
         """
