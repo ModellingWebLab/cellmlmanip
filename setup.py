7628347b26238775ec575a89c300b1af93e92435
MauriceHendrix <52317399+MauriceHendrix@users.noreply.github.com>
2020-06-08 21:04:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ from setuptools import find_packages, setup
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license_ = f.read()
-
 setup(
     name='cellmlmanip',
     version='0.1.0',
     description='CellML loading and model equation manipulation',
     long_description=readme,
+    long_description_content_type="text/markdown",
     author='Asif U Tamuri, Sarah M Keating, Maurice Hendrix, Michael Clerx, Jonathan Cooper',
     author_email='j.p.cooper@ucl.ac.uk',
     url='https://github.com/ModellingWebLab/cellmlmanip',
-    license=license_,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+
     packages=find_packages(exclude=('tests', 'docs')),
     include_package_data=True,
     python_requires='>=3.5',
