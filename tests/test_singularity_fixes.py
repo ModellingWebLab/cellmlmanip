e9975b18e240744d3476e0fdf0fa7057615207db
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2021-03-19 16:39:14 +0000

    improved code coverage

--- tests/test_singularity_fixes.py
+++ tests/test_singularity_fixes.py
@@ -1,4 +1,5 @@
-import pytest
+import os
+
 from sympy import (
     Function,
     Piecewise,
@@ -6,8 +7,9 @@ from sympy import (
     log,
 )
 
-from cellmlmanip._singularity_fixes import _remove_singularities
 from cellmlmanip.model import Quantity, Variable
+from cellmlmanip.parser import Transpiler
+from cellmlmanip.printer import Printer
 
 from . import shared
 
@@ -20,16 +22,7 @@ V = Variable(name='V', units='millivolt')
 Z = to_quant(6.2)
 U = to_quant(2) * V + to_quant(5.0)
 SP = (-(log(Z) + to_quant(5.0)) / to_quant(2))
-
-
-@pytest.fixture(scope='session')
-def expr1():
-    return (Z * V - Z * SP) / (exp(U) * -Z + to_quant(1.0))
-
-
-@pytest.fixture(scope='session')
-def expr2():
-    return (Z * V - Z * SP) / (exp(U) * Z - to_quant(1.0))
+P = Printer()
 
 
 class exp_(Function):
@@ -45,107 +38,11 @@ class exp_(Function):
         return self
 
 
-def test_no_singularity():
-    expr = (exp_(U) * -Z + to_quant(1.0)) / to_quant(25.0)
-    changed, new_ex = _remove_singularities(expr, V)
-    assert str(_remove_singularities(expr, V)) == str((False, expr))
-
-
-def test_no_singularity2():
-    expr = to_quant(1) / (to_quant(1) + exp_(to_quant(1.5669291338582676) - to_quant(0.07874015748031496) * V))
-    assert str(_remove_singularities(expr, V)) == str((False, expr))
-
-
-def test_remove_singularities1(expr1):
-    assert str(_remove_singularities(expr1, V)) == ('(True, Piecewise(((-_-3.4122746960255230 + _V)*(-(_-3.41227469602'
-                                                    '55230*_6.2 - _6.2*(-_5 - log(_6.2))/_2)/(_1 - _6.2*exp(_-3.412274'
-                                                    '6960255230*_2 + _5)) + (_-3.4122745960255229*_6.2 - _6.2*(-_5 - l'
-                                                    'og(_6.2))/_2)/(_1 - _6.2*exp(_-3.4122745960255229*_2 + _5)))/(_-3'
-                                                    '.4122745960255229 - _-3.4122746960255230) + (_-3.4122746960255230'
-                                                    '*_6.2 - _6.2*(-_5 - log(_6.2))/_2)/(_1 - _6.2*exp(_-3.41227469602'
-                                                    '55230*_2 + _5)), Abs(_-3.4122746460255229 - _V) < Abs(_-3.4122745'
-                                                    '960255229/2 - _-3.4122746960255230/2)), ((_6.2*_V - _6.2*(-_5 - l'
-                                                    'og(_6.2))/_2)/(_1 - _6.2*exp(_2*_V + _5)), True)))')
-    assert str(_remove_singularities(expr1, V, U_offset=1e-5)) == ('(True, Piecewise(((-_-3.4122796460255230 + _V)*(-('
-                                                                   '_-3.4122796460255230*_6.2 - _6.2*(-_5 - log(_6.2))'
-                                                                   '/_2)/(_1 - _6.2*exp(_-3.4122796460255230*_2 + _5))'
-                                                                   ' + (_-3.4122696460255229*_6.2 - _6.2*(-_5 - log(_6'
-                                                                   '.2))/_2)/(_1 - _6.2*exp(_-3.4122696460255229*_2 + '
-                                                                   '_5)))/(_-3.4122696460255229 - _-3.4122796460255230'
-                                                                   ') + (_-3.4122796460255230*_6.2 - _6.2*(-_5 - log(_'
-                                                                   '6.2))/_2)/(_1 - _6.2*exp(_-3.4122796460255230*_2 +'
-                                                                   ' _5)), Abs(_-3.4122746460255229 - _V) < Abs(_-3.41'
-                                                                   '22696460255229/2 - _-3.4122796460255230/2)), ((_6.'
-                                                                   '2*_V - _6.2*(-_5 - log(_6.2))/_2)/(_1 - _6.2*exp(_'
-                                                                   '2*_V + _5)), True)))')
-    assert str(_remove_singularities(expr1, V, exp_function=exp_)) == ('(False, (_6.2*_V - _6.2*(-_5 - log(_6.2))/_2)/'
-                                                                       '(_1 - _6.2*exp(_2*_V + _5)))')
-    expr = expr1.replace(exp, exp_)
-    assert str(_remove_singularities(expr, V, exp_function=exp_)) == ('(True, Piecewise(((-_-3.4122746960255230 + _V)*'
-                                                                      '(-(_-3.4122746960255230*_6.2 - _6.2*(-_5 - log('
-                                                                      '_6.2))/_2)/(_1 - _6.2*exp_(_-3.4122746960255230'
-                                                                      '*_2 + _5)) + (_-3.4122745960255229*_6.2 - _6.2*'
-                                                                      '(-_5 - log(_6.2))/_2)/(_1 - _6.2*exp_(_-3.41227'
-                                                                      '45960255229*_2 + _5)))/(_-3.4122745960255229 - '
-                                                                      '_-3.4122746960255230) + (_-3.4122746960255230*_'
-                                                                      '6.2 - _6.2*(-_5 - log(_6.2))/_2)/(_1 - _6.2*exp'
-                                                                      '_(_-3.4122746960255230*_2 + _5)), Abs(_-3.41227'
-                                                                      '46460255229 - _V) < Abs(_-3.4122745960255229/2 '
-                                                                      '- _-3.4122746960255230/2)), ((_6.2*_V - _6.2*(-'
-                                                                      '_5 - log(_6.2))/_2)/(_1 - _6.2*exp_(_2*_V + _5)'
-                                                                      '), True)))')
-
-
-def test_remove_singularities2(expr2):
-    assert str(_remove_singularities(expr2, V)) == ('(True, Piecewise(((-_-3.4122746960255230 + _V)*(-(_-3.41227469602'
-                                                    '55230*_6.2 - _6.2*(-_5 - log(_6.2))/_2)/(-_1 + _6.2*exp(_-3.41227'
-                                                    '46960255230*_2 + _5)) + (_-3.4122745960255229*_6.2 - _6.2*(-_5 - '
-                                                    'log(_6.2))/_2)/(-_1 + _6.2*exp(_-3.4122745960255229*_2 + _5)))/(_'
-                                                    '-3.4122745960255229 - _-3.4122746960255230) + (_-3.41227469602552'
-                                                    '30*_6.2 - _6.2*(-_5 - log(_6.2))/_2)/(-_1 + _6.2*exp(_-3.41227469'
-                                                    '60255230*_2 + _5)), Abs(_-3.4122746460255229 - _V) < Abs(_-3.4122'
-                                                    '745960255229/2 - _-3.4122746960255230/2)), ((_6.2*_V - _6.2*(-_5 '
-                                                    '- log(_6.2))/_2)/(-_1 + _6.2*exp(_2*_V + _5)), True)))')
-
-
-def test_remove_singularities3(expr1):
-    assert str(_remove_singularities(1 / expr1, V)) == ('(True, Piecewise(((-_-3.4122746960255230 + _V)*((_1 - _6.2*ex'
-                                                        'p(_-3.4122745960255229*_2 + _5))/(_-3.4122745960255229*_6.2 -'
-                                                        ' _6.2*(-_5 - log(_6.2))/_2) - (_1 - _6.2*exp(_-3.412274696025'
-                                                        '5230*_2 + _5))/(_-3.4122746960255230*_6.2 - _6.2*(-_5 - log(_'
-                                                        '6.2))/_2))/(_-3.4122745960255229 - _-3.4122746960255230) + (_'
-                                                        '1 - _6.2*exp(_-3.4122746960255230*_2 + _5))/(_-3.412274696025'
-                                                        '5230*_6.2 - _6.2*(-_5 - log(_6.2))/_2), Abs(_-3.4122746460255'
-                                                        '229 - _V) < Abs(_-3.4122745960255229/2 - _-3.4122746960255230'
-                                                        '/2)), ((_1 - _6.2*exp(_2*_V + _5))/(_6.2*_V - _6.2*(-_5 - log'
-                                                        '(_6.2))/_2), True)))')
-
-
-def test_remove_singularities4(expr2):
-    assert str(_remove_singularities(1 / expr2, V)) == ('(True, Piecewise(((-_-3.4122746960255230 + _V)*((-_1 + _6.2*e'
-                                                        'xp(_-3.4122745960255229*_2 + _5))/(_-3.4122745960255229*_6.2 '
-                                                        '- _6.2*(-_5 - log(_6.2))/_2) - (-_1 + _6.2*exp(_-3.4122746960'
-                                                        '255230*_2 + _5))/(_-3.4122746960255230*_6.2 - _6.2*(-_5 - log'
-                                                        '(_6.2))/_2))/(_-3.4122745960255229 - _-3.4122746960255230) + '
-                                                        '(-_1 + _6.2*exp(_-3.4122746960255230*_2 + _5))/(_-3.412274696'
-                                                        '0255230*_6.2 - _6.2*(-_5 - log(_6.2))/_2), Abs(_-3.4122746460'
-                                                        '255229 - _V) < Abs(_-3.4122745960255229/2 - _-3.4122746960255'
-                                                        '230/2)), ((-_1 + _6.2*exp(_2*_V + _5))/(_6.2*_V - _6.2*(-_5 -'
-                                                        ' log(_6.2))/_2), True)))')
-
-
-def test_remove_singularities5(expr1):  # Try with numbers in place of Quantity Dummies
-    expr = expr1.xreplace({v: float(str(v)) for v in expr1.atoms(Quantity)})
-    print(_remove_singularities(expr, V))
-    assert str(_remove_singularities(expr, V)) == ('(True, Piecewise(((-_-3.41227469602552 + _V)*(-(6.2*_-3.4122746960'
-                                                   '2552 + 21.1561028053582)/(1.0 - 920.161586435975*exp(2.0*_-3.41227'
-                                                   '469602552)) + (6.2*_-3.41227459602552 + 21.1561028053582)/(1.0 - 9'
-                                                   '20.161586435975*exp(2.0*_-3.41227459602552)))/(_-3.41227459602552 '
-                                                   '- _-3.41227469602552) + (6.2*_-3.41227469602552 + 21.1561028053582'
-                                                   ')/(1.0 - 920.161586435975*exp(2.0*_-3.41227469602552)), Abs(_-3.41'
-                                                   '227464602552 - _V) < Abs(_-3.41227459602552/2 - _-3.41227469602552'
-                                                   '/2)), ((6.2*_V + 21.1561028053582)/(1.0 - 920.161586435975*exp(2.0'
-                                                   '*_V)), True)))')
+def test_no_V(caplog):
+    model = shared.load_model('test_no_V_no_odes')
+    model.remove_fixable_singularities()
+    assert 'WARNING' in caplog.text
+    assert "Has no membrane_voltage tagged, cannot remove fixable singuarities." in caplog.text
 
 
 def test_fix_singularity_equations():
@@ -172,14 +69,39 @@ def test_fix_singularity_equations():
 
 
 def test_fix_singularity_equations2():
-    # check fixes via load_model
-    model = shared.load_model('bondarenko_szigeti_bett_kim_rasmusson_2004_apical')
+    model = shared.load_model('beeler_reuter_model_1977')
+
     old_piecewises = tuple(str(eq.lhs) for eq in model.equations if eq.rhs.has(Piecewise))
     assert len(old_piecewises) == 1
 
     model.remove_fixable_singularities()
     new_piecewises = tuple(eq.lhs for eq in model.equations if eq.rhs.has(Piecewise))
+    assert len(new_piecewises) == 3
+
+    additional_piecewises = [v for v in new_piecewises if str(v) not in old_piecewises]
+    assert str(additional_piecewises) == '[_sodium_current_m_gate$alpha_m, _time_independent_outward_current$i_K1]'
+
+
+def test_fix_singularity_equations3():
+    # check this still works when the parser is set to generate a differente exp function
+    Transpiler.set_mathml_handler('exp', exp_)  # restore exp function
+    model = shared.load_model('bondarenko_szigeti_bett_kim_rasmusson_2004_apical')
+    old_piecewises = tuple(str(eq.lhs) for eq in model.equations if eq.rhs.has(Piecewise))
+    assert len(old_piecewises) == 1
+    assert str([eq.rhs for eq in model.equations if str(eq.lhs) ==
+                'slow_delayed_rectifier_potassium_current$alpha_n'][0] ==
+           '_4.81333e-06*(_26.5 + _membrane$V)/(_1 - exp_(-_0.128*(_26.5 + _membrane$V)))')
+
+    model.remove_fixable_singularities()
+    Transpiler.set_mathml_handler('exp', exp)  # restore exp function
+
+    new_piecewises = tuple(eq.lhs for eq in model.equations if eq.rhs.has(Piecewise))
     assert len(new_piecewises) == 2
 
     additional_piecewises = [v for v in new_piecewises if str(v) not in old_piecewises]
     assert str(additional_piecewises) == '[_slow_delayed_rectifier_potassium_current$alpha_n]'
+
+    expected = open(os.path.join(os.path.dirname(__file__), 'singularity.txt'), 'r').read()
+
+    assert str([eq.rhs for eq in model.equations if str(eq.lhs) ==
+                'slow_delayed_rectifier_potassium_current$alpha_n'][0]) == expected
