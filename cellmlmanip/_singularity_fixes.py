1d14f160dd25b81e4e1f83089a5ef87ba9c9c0fe
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2021-03-22 16:05:47 +0000

    improved documentation

--- cellmlmanip/_singularity_fixes.py
+++ cellmlmanip/_singularity_fixes.py
@@ -1,5 +1,5 @@
 """
-`remove_fixable_singularities` specifies a method to remove fixable singularities from a model
+``remove_fixable_singularities`` specifies a method to remove fixable singularities from a model
 This represents a flattened CellML model and metadata about its variables.
 """
 from math import isclose
@@ -27,25 +27,38 @@ from cellmlmanip.model import FLOAT_PRECISION
 ONE = cellmlmanip.Quantity(1.0, 'dimensionless')
 
 
-def _generate_piecewise(vs, ve, sp, ex, V):
-    """Generates a piecewise for expression based on the singularity point (sp) and vmin (vs) and vmax (ve) """
-    if vs is None:  # This shouldn't be apiecewise since we have no Vmin / Vmax
-        return ex
+def _generate_piecewise(expr, V, sp, Vmin, Vmax):
+    """Generates a new (piecewise) expression based on expr with a linear interpolation when V is between Vmin and Vmax
 
-    f_vs = ex.xreplace({V: vs})
-    f_ve = ex.xreplace({V: ve})
-    return Piecewise((f_vs + ((V - vs) / (ve - vs)) * (f_ve - f_vs),
-                      Abs(V - sp) < Abs((ve - vs) / 2)), (ex, True))
+       The returned expression between Vmin and Vmax is::
+       f(vs) + (f(ve) - f(vs)) * (V - vs) / (ve - vs)
+       whenever sp is not Null, returns the original expression otherwise.
+
+       :param: The expression to turn into a piecewise
+       :param: V the voltage variable.
+       :param: the value of the singularity point.
+       :param: the value of the lower bound.
+       :param: the value of the upper bound.
+       :return: expr with a singularity exception around sp, if sp is not None else return the original expr
+    """
+    if sp is None:  # This shouldn't be apiecewise since we have no Vmin / Vmax
+        return expr
+
+    f_Vmin = expr.xreplace({V: Vmin})
+    f_Vmax = expr.xreplace({V: Vmax})
+    return Piecewise((f_Vmin + ((V - Vmin) / (Vmax - Vmin)) * (f_Vmax - f_Vmin),
+                      Abs(V - sp) < Abs((Vmax - Vmin) / 2)), (expr, True))
 
 
-def _get_sinularity(expr, V, U_offset, exp_function):
-    """Finds U_offset and teh singularity either side and singularity U in ghk-like equations
-       these are of one of the the following forms where U is an expression over V:
-       - `U / (exp(U) - 1.0)`
-       - `U / (1.0 - exp(U))`
-       - `(exp(U) - 1.0) / U`
-       - `(1.0 - exp(U)) / U`
-       :return: (Vmin (vs), Vmax (ve), singularity point)
+def _get_singularity(expr, V, U_offset, exp_function):
+    """ Finds singularities in equations of the form::
+       - ``U / (exp(U) - 1.0)``
+       - ``U / (1.0 - exp(U))``
+       - ``(exp(U) - 1.0) / U``
+       - ``(1.0 - exp(U)) / U``
+
+       In addition to the singularity itself, this functions returns a lower and upper bounds::
+       :return: (Vmin, Vmax, singularity point)
        """
     Z = Wild('Z', real=True)
     U_wildcard = Wild('U_wildcard', real=True, include=[V])
@@ -61,7 +74,7 @@ def _get_sinularity(expr, V, U_offset, exp_function):
                                                             and isclose(m[SP_wildcard], sp)))
 
     def float_dummies(expr):
-        """Turns flaots back into Quantity dummies to be in line with the rest of the sympy equations"""
+        """Turns floats back into Quantity dummies to be in line with the rest of the sympy equations"""
         if expr is None:
             return None
         return expr.xreplace({f: cellmlmanip.Quantity(f, 'dimensionless') for f in expr.atoms(Float)})
@@ -74,7 +87,7 @@ def _get_sinularity(expr, V, U_offset, exp_function):
     if len(denominator) == 0 or len(numerator) == 0 or not expr.has(exp_function):
         return None, None, None
 
-    (vs, ve, sp) = None, None, None
+    (Vmin, Vmax, sp) = None, None, None
     # U might be on top, try numerator / denominator and denominator / numerator
     for num, denom in ((numerator, denominator), (denominator, numerator)):
         found_on_top = False
@@ -90,7 +103,7 @@ def _get_sinularity(expr, V, U_offset, exp_function):
                         # Find the singularity point by solving u for V==0, excluding irratinal results
                         sp = tuple(filter(lambda s: not s.has(I), solveset(u, V)))
                         if sp:
-                            # we found a singularity point, now find vs,ve (or vmin, vmax)
+                            # we found a singularity point, now find Vmin, Vmax
                             # by solving for U_offset either side of U
                             find_v_low = solveset(u + U_offset, V)
                             find_v_up = solveset(u - U_offset, V)
@@ -98,11 +111,11 @@ def _get_sinularity(expr, V, U_offset, exp_function):
                             find_v_up = tuple(find_v_up)
                             assert find_v_low and find_v_up and len(find_v_low) == len(find_v_up) == len(sp) == 1, \
                                 'Expecting exactly 1 singularity point '
-                            (vs, ve, sp) = (find_v_low[0], find_v_up[0], sp[0])
+                            (Vmin, Vmax, sp) = (find_v_low[0], find_v_up[0], sp[0])
                     except TypeError:
                         pass  # Result could be 'ConditionSet' which is not iterable and not Real
 
-                if vs is not None:  # check top
+                if Vmin is not None:  # check top
                     for n in num:  # Check arguments in numerator (or denominator)
                         match = n.match(Z * V - Z * SP_wildcard)  # search for a multiple of V - sp
                         found_on_top = check_top_match(match, sp)
@@ -115,25 +128,26 @@ def _get_sinularity(expr, V, U_offset, exp_function):
                     if found_on_top:  # found singularity, no need to try further
                         break
                     else:
-                        (vs, ve, sp) = None, None, None
-        if vs is not None and found_on_top:  # found singularity, no need to try further
+                        (Vmin, Vmax, sp) = None, None, None
+        if Vmin is not None and found_on_top:  # found singularity, no need to try further
             break
         else:
-            (vs, ve, sp) = None, None, None
+            (Vmin, Vmax, sp) = None, None, None
 
     # Put dummies back in and return the singularity point and range bundries
-    return (float_dummies(vs), float_dummies(ve), float_dummies(sp))
+    return (float_dummies(Vmin), float_dummies(Vmax), float_dummies(sp))
 
 
 def _fixe_expr_parts(expr, V, U_offset, exp_function):
-    """Removes fixable singularities and replaces it with a piecewise
-    :return: either (Vmin (vs), Vmax (ve), singularity point, expression, True)
+    """Removes fixable singularities and replaces them with piecewise expressions.
+
+    :return: either (Vmin, Vmax, singularity point, expression, True)
                     if we have identified a singularity needs to be made but it can't be done yet.
                     For eample if 2 singularities with the same singularity point are added up,
                     a single singularity is constructed instead.
-             or `(None, None, None, fixed expr, fixed_expr has piecewise)` otherwise
+             or ``(None, None, None, fixed expr, fixed_expr has piecewise)`` otherwise
 
-    see :meth:fix_singularity_equations for more details "
+    see :meth:`fix_singularity_equations for more details` "
     """
 
     if isinstance(expr, Mul):  # 1 * A --> A (remove unneeded 1 *)
@@ -153,37 +167,37 @@ def _fixe_expr_parts(expr, V, U_offset, exp_function):
             new_expr_parts.append(_fixe_expr_parts(a, V, U_offset, exp_function))
 
         # If all arguments have the same singularity point, return 1 singularity with the widest range
-        range = [item for (vs, ve, _, _, _) in new_expr_parts for item in (vs, ve)]
+        range = [item for (Vmin, Vmax, _, _, _) in new_expr_parts for item in (Vmin, Vmax)]
         if len(new_expr_parts) > 1 and len(set([str(sp) for (_, _, sp, _, _) in new_expr_parts])) == 1 \
                 and all(isinstance(b, cellmlmanip.Quantity) for b in range):
             sp = new_expr_parts[0][2]
-            vs, ve = min(range, key=lambda v: float(str(v))), max(range, key=lambda v: float(str(v)))
-            return (vs, ve, sp, expr, True)
+            Vmin, Vmax = min(range, key=lambda v: float(str(v))), max(range, key=lambda v: float(str(v)))
+            return (Vmin, Vmax, sp, expr, True)
         else:  # Singularity points differ, so create each piecewise seperately and add them up
             expr_parts = []
             is_piecewise = False
-            for vs, ve, sp, ex, has_piecewise in new_expr_parts:
-                is_piecewise = is_piecewise or has_piecewise or vs is not None
-                expr_parts.append(_generate_piecewise(vs, ve, sp, ex, V))
+            for Vmin, Vmax, sp, ex, has_piecewise in new_expr_parts:
+                is_piecewise = is_piecewise or has_piecewise or Vmin is not None
+                expr_parts.append(_generate_piecewise(ex, V, sp, Vmin, Vmax))
             return (None, None, None, Add(*expr_parts), is_piecewise)
 
     elif isinstance(expr, Pow) and expr.args[1] == -1.0 and len(expr.args) == 2:  # 1/A
         # Find singularities in A and adjust result to represent 1 / A
-        vs, ve, sp, ex, has_piecewise = _fixe_expr_parts(expr.args[0], V, U_offset, exp_function)
-        has_piecewise = has_piecewise or vs is not None
-        return (None, None, None, ONE / _generate_piecewise(vs, ve, sp, ex, V), has_piecewise)
+        Vmin, Vmax, sp, ex, has_piecewise = _fixe_expr_parts(expr.args[0], V, U_offset, exp_function)
+        has_piecewise = has_piecewise or Vmin is not None
+        return (None, None, None, ONE / _generate_piecewise(ex, V, sp, Vmin, Vmax), has_piecewise)
 
     elif isinstance(expr, Mul):  # A * B * ...
-        (vs, ve, sp) = _get_sinularity(check_U_expr, V, U_offset, exp_function)  # Find the singularity point
-        if vs is not None:
-            return (vs, ve, sp, expr, True)
+        (Vmin, Vmax, sp) = _get_singularity(check_U_expr, V, U_offset, exp_function)  # Find the singularity point
+        if sp is not None:
+            return (Vmin, Vmax, sp, expr, True)
         else:  # Couldn't find singularity, try the expression's arguments
             expr_parts = []
             is_piecewise = False
             for sub_ex in expr.args:
-                vs, ve, sp, ex, has_piecewise = _fixe_expr_parts(sub_ex, V, U_offset, exp_function)
-                has_piecewise = is_piecewise = is_piecewise or has_piecewise or vs is not None
-                expr_parts.append(_generate_piecewise(vs, ve, sp, ex, V))
+                Vmin, Vmax, sp, ex, has_piecewise = _fixe_expr_parts(sub_ex, V, U_offset, exp_function)
+                has_piecewise = is_piecewise = is_piecewise or has_piecewise or Vmin is not None
+                expr_parts.append(_generate_piecewise(ex, V, sp, Vmin, Vmax))
             return (None, None, None, Mul(*expr_parts), is_piecewise)
 
     else:  # Different type of equation e.g a number
@@ -198,13 +212,13 @@ def _remove_singularities(expr, V, U_offset=1e-7, exp_function=exp):
     :param: exp_function the function representing exp
     :return: (bool, expr) as follows: (expr has changed, expr with singularities fixes if appropriate)
 
-    see :meth:fix_singularity_equations for more details "
+    see :meth:`fix_singularity_equations for more details `"
     """
     if not expr.has(exp_function):
         return False, expr
-    (vs, ve, sp, ex, changed) = _fixe_expr_parts(expr, V, U_offset, exp_function)
-    ex = _generate_piecewise(vs, ve, sp, ex, V)
-    return changed or vs is not None, ex
+    (Vmin, Vmax, sp, ex, changed) = _fixe_expr_parts(expr, V, U_offset, exp_function)
+    ex = _generate_piecewise(ex, V, sp, Vmin, Vmax)
+    return changed or Vmin is not None, ex
 
 
 def remove_fixable_singularities(model, V, modifiable_parameters, U_offset=1e-7, exp_function=exp):
